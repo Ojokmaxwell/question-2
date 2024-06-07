b3384c8153153f11691f6687f7e103cf8b1fbf23
Ojokmaxwell <maxfinadagreat@gmail.com>
2024-06-07 14:52:46 +0300

    Add files via upload

---
+++ courses.java
@@ -0,0 +1,11 @@
+public class Course {
+    public String name;
+    public String code;
+    public int tuition;
+
+    public Course(String name, String code, int tuition) {
+        this.name = name;
+        this.code = code;
+        this.tuition = tuition;
+    }
+}
